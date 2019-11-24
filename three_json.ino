cd5dea0d5b109356942ae587675f7507bf61fa69
TTurbo0824 <429why@gmail.com>
2019-11-24 13:27:00 -0500

    Create three_json.ino

---
+++ three_json.ino
@@ -0,0 +1,79 @@
+//Libraries
+#include <ArduinoJson.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 8
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+
+//Variables
+int waterPin = A0;
+int lightPin = 12;
+int fanPin = 13;
+//int incomingByte;
+String input;
+
+float humF;
+int humI;
+float tempF;
+int tempI;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(lightPin, OUTPUT);
+  pinMode(waterPin, OUTPUT);
+  pinMode(fanPin, OUTPUT);
+
+  while (!Serial.available()) { // until the server responds,
+    Serial.println("hello");    // send a hello message
+    delay(500);                 // every half second
+  }
+  dht.begin();
+}
+
+void loop() {
+  //  delay(2000);
+  DynamicJsonDocument doc(1024);
+
+  if (Serial.available() > 0) {   // see if there's incoming serial data
+    input = Serial.readStringUntil('\n');
+    deserializeJson(doc, input);
+    int lightTime = doc["data"][0];
+    int waterTime = doc["data"][1];
+    int fanTime = doc["data"][2];
+
+    lightBlinky(lightTime);
+    waterBlinky(waterTime);
+    fanBlinky(fanTime);
+  }
+
+  humF = dht.readHumidity() * 100;
+  humI = (int)humF;
+  tempF = dht.readTemperature() * 100;
+  tempI = (int)tempF;
+
+  Serial.print(humI);
+  Serial.print(",");
+  Serial.println(tempI);
+  delay(30);
+}
+
+void lightBlinky(int time) {
+  digitalWrite(lightPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(lightPin, LOW);
+}
+
+void waterBlinky(int time) {
+  digitalWrite(waterPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(waterPin, LOW);
+}
+
+void fanBlinky(int time) {
+  digitalWrite(fanPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(fanPin, LOW);
+}
