36b015f062b83187d184a8912184209b839ac692
TTurbo0824 <429why@gmail.com>
2019-11-24 12:12:58 -0500

    Update three_serial.ino

--- three_serial.ino
+++ three_serial.ino
@@ -4,12 +4,12 @@
 
 //Constants
 #define DHTPIN 8     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 //Variables
-int pumpPin = A0;
-int lampPin = 12;
+int waterPin = A0;
+int lightPin = 12;
 int fanPin = 13;
 int incomingByte;
 float humF;
@@ -19,8 +19,8 @@ int tempI;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(pumpPin, OUTPUT);
-  pinMode(lampPin, OUTPUT);
+  pinMode(lightPin, OUTPUT);
+  pinMode(waterPin, OUTPUT);
   pinMode(fanPin, OUTPUT);
 
   while (!Serial.available()) { // until the server responds,
@@ -33,9 +33,9 @@ void setup() {
 void loop() {
   //  delay(2000);
   if (Serial.available() > 0) {   // see if there's incoming serial data
-    incomingByte = Serial.read(); 
+    incomingByte = Serial.read();
     if (incomingByte > 20) {
-      lampBlinky(10); 
+      lightBlinky(10);
     } else if (incomingByte > 30) {
       fanBlinky(5);
     }
@@ -49,23 +49,23 @@ void loop() {
   Serial.print(humI);
   Serial.print(",");
   Serial.println(tempI);
-  delay(30); 
+  delay(30);
+}
+
+void lightBlinky(int time) {
+  digitalWrite(lightPin, HIGH);
+  delay(time*1000);
+  digitalWrite(lightPin, LOW);
+}
+
+void waterBlinky(int time) {
+  digitalWrite(waterPin, HIGH);
+  delay(time*1000);
+  digitalWrite(waterPin, LOW);
 }
 
 void fanBlinky(int time) {
   digitalWrite(fanPin, HIGH);
   delay(time*1000);
   digitalWrite(fanPin, LOW);
-}
-
-void lampBlinky(int time) {
-  digitalWrite(lampPin, HIGH);
-  delay(time*1000);
-  digitalWrite(lampPin, LOW);
-}
-
-void pumpBlinky(int time) {
-  digitalWrite(pumpPin, HIGH);
-  delay(time*1000);
-  digitalWrite(pumpPin, LOW);
 }
