147e8dd810c53f3773f8ac7180c73676364ef91c
TTurbo0824 <429why@gmail.com>
2019-11-24 12:12:14 -0500

    Create arJSON_test.ino

---
+++ arJSON_test.ino
@@ -0,0 +1,51 @@
+#include <ArduinoJson.h>
+
+int waterPin = A0;
+int lightPin = 12;
+int fanPin = 13;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(lightPin, OUTPUT);
+  pinMode(waterPin, OUTPUT);
+  pinMode(fanPin, OUTPUT);
+
+  const char* input = "{\"name\":\"taiwan\",\"data\":[10, 5, 0]}";
+  StaticJsonDocument<256> doc;
+  DeserializationError err = deserializeJson(doc, input);
+
+  if (err) {
+    Serial.print("Error: ");
+    Serial.println(err.c_str());
+    return;
+  }
+  int lightTime = doc["data"][0];
+  int waterTime = doc["data"][1];
+  int fanTime = doc["data"][2];
+  lightBlinky(lightTime);
+  waterBlinky(waterTime);
+  fanBlinky(fanTime);
+}
+void lightBlinky(int time) {
+  digitalWrite(lightPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(lightPin, LOW);
+}
+
+void waterBlinky(int time) {
+  digitalWrite(waterPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(waterPin, LOW);
+}
+
+void fanBlinky(int time) {
+  digitalWrite(fanPin, HIGH);
+  delay(time * 1000);
+  digitalWrite(fanPin, LOW);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
