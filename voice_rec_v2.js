860b6d7264ca8235b7f007008201cf10e339e6d7
TTurbo0824 <429why@gmail.com>
2019-11-25 06:20:51 -0500

    Update voice_rec_v2.js

--- voice_rec_v2.js
+++ voice_rec_v2.js
@@ -1,10 +1,12 @@
 // inspired by Daniel Shiffman's code with texrt video
-let libra = ['yellow', 'ribbon', 'march', 'belief', 'gray', 'apple', 'data', 'escape', 'side', 'candle', taiwan', 'voice', 'revolution', 'rainbow', 'power', 'support', 'peace', 'cold', 'indifferent'];
+let libra = ['yellow', 'ribbon', 'march', 'belief', 'gray', 'apple', 'data', 'escape', 'side', 'candle', 'taiwan', 'voice', 'revolution', 'rainbow', 'power', 'support', 'peace', 'cold', 'indifferent'];
 let words = [];
 let occurrences = [];
 let lang, speechRec;
 let w;
-let options = { baudRate: 115200};
+let options = {
+  baudRate: 115200
+};
 let serial; // variable to hold an instance of the serialport library
 let portName = '/dev/ttyACM0'; // fill in your serial port name here
 let inData; // for incoming serial data
@@ -13,7 +15,6 @@ let sensors;
 function serverConnected() {
   console.log('connected to server.');
 }
-
 
 function serialError(err) {
   console.log('Something went wrong with the serial port.');
@@ -26,64 +27,64 @@ function serialEvent() {
     sensors = split(inData, ',');
     console.log(sensors);
 
-  if (sensors.length > 1) {
+    if (sensors.length > 1) {
       fill(0);
-      rect(0,height/2, width, height);
+      rect(0, height / 2, width, height);
       fill(255);
       textSize(32);
-      text("humidity: " + sensors[0] / 100 + '%, temperature: ' + sensors[1] / 100 + '%', width/5,2*height/3 );
+      text("humidity: " + sensors[0] / 100 + '%, temperature: ' + sensors[1] / 100 + '%', width / 5, 2 * height / 3);
     }
   }
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    lang = 'en-US';
-    speechRec = new p5.SpeechRec(lang, gotSpeech);
-    let continuous = true;
-    let interim = true;
-    speechRec.start(continuous, interim); 
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  lang = 'en-US';
+  speechRec = new p5.SpeechRec(lang, gotSpeech);
+  let continuous = true;
+  let interim = true;
+  speechRec.start(continuous, interim);
 
-    // serial
-    serial = new p5.SerialPort();
-    serial.on('list', printList);
-    serial.open(portName, options);
-    serial.on('data', serialEvent);
-    serial.on('connected', serverConnected);
-    serial.on('error', serialError);
-  }
-  
+  // serial
+  serial = new p5.SerialPort();
+  serial.on('list', printList);
+  serial.open(portName, options);
+  serial.on('data', serialEvent);
+  serial.on('connected', serverConnected);
+  serial.on('error', serialError);
+}
+
 function gotSpeech() {
-    if (speechRec.resultValue) {
-      //createP(speechRec.resultString);
-      w = speechRec.resultString;
-      if(w !== words[words.length-1]){
+  if (speechRec.resultValue) {
+    //createP(speechRec.resultString);
+    w = speechRec.resultString;
+    if (w !== words[words.length - 1]) {
       words.push(w);
-        for (let  a = 0,  b = words.length; a < b; a++) {
-          occurrences[words[a]] = (occurrences[words[a]] || 0) + 1;
-          }
-
-      for (let i = 0; i < libra.length; i++){
-      if (words[words.length-1] == libra[i]){
-      noStroke();
-      fill(0);
-      rect(0, 0, width, height/2);
-      fill(255);
-      textSize(70);
-      text(words[words.length-1], width/5, height/3);
-      serial.write(72);
-	console.log("got from ard");
-      }else{
-        serial.write(0);
-}
-}
-    }
-    }
-  }
-
-  function printList(portList) {
-    for (let i = 0; i < portList.length; i++) {
-      console.log(i + " " + portList[i]);
+	    
+      for (let a = 0, b = words.length; a < b; a++) {
+        occurrences[words[a]] = (occurrences[words[a]] || 0) + 1;
+      }
+      for (let i = 0; i < libra.length; i++) {
+        if (words[words.length - 1] == libra[i]) {
+          noStroke();
+          fill(0);
+          rect(0, 0, width, height / 2);
+          fill(255);
+          textSize(70);
+          text(words[words.length - 1], width / 5, height / 3);
+          serial.write(72);
+          console.log("got from ard");
+        } else {
+          serial.write(0);
+        }
+      }
     }
   }
+}
+
+function printList(portList) {
+  for (let i = 0; i < portList.length; i++) {
+    console.log(i + " " + portList[i]);
+  }
+}
